0da2e4d81795891ea15932dc6a8b69560efaf2f3
Daniel Gavrilov <drifter4e@gmail.com>
2014-01-27 03:53:34 +0000

    Remove unnecessary dependencies

--- app/index.js
+++ app/index.js
@@ -1,6 +1,4 @@
-var path = require('path');
 var express = require('express');
-var logfmt = require('logfmt');
 var api = require('./api');
 var site = require('./site');
 
@@ -9,7 +7,7 @@ var port = process.env.PORT || 8000;
 
 app.use(express.favicon());
 app.configure('production', function() {
-  app.use(logfmt.requestLogger());
+  app.use(require('logfmt').requestLogger());
 });
 app.configure('development', function() {
   app.use(express.logger('dev'));
