d8bb43db0765aace8d7ac46a3a5e6038639b9213
Daniel Gavrilov <drifter4e@gmail.com>
2014-01-28 18:33:40 +0000

    Initialize router in main.js

--- public/js/components/router.js
+++ public/js/components/router.js
@@ -1,12 +1,12 @@
-app.router = new (Backbone.Router.extend({
+app.routers.main = Backbone.Router.extend({
   routes: {
     '': 'search',
   },
   search: function(params) {
     params = params || {};
-    var query = params.q || params.postcode;
-    var location = params.lng && params.lat && [params.lng, params.lat];
+    var query = params.q;
+    var location = (params.lng != null) && (params.lat != null) && [params.lng, params.lat];
     if (query) app.search.query(query);
     else if (location) app.search.byLocation(location);
   },
-}));
+});
--- public/js/main.js
+++ public/js/main.js
@@ -1,51 +1,43 @@
-// (function(undefined) {
-
-var $body = $(document.body);
-
-if ('geolocation' in navigator) {
-  $body.addClass('geolocation');
-}
-
-// scrolling performance problems
-// $('.schools').find('table').stickyTableHeaders();
-
-// Colors
 app.colors = new app.constructors.colors(app.preload.colornames);
-
-// Schools
 app.cache = new app.collections.schools(null, {
   names: app.preload.schoolnames
 });
-
-app.search = new app.views.search({
-  el: $('.search-form')
-});
-
-// Subjects
 app.subjects = new app.collections.subjects(app.preload.subjectnames.sort().map(function(name) {
   return {name: name};
 }));
-app.subjects.view = new app.views.subjects({
-  el: $('.subjects-filter'),
-  collection: app.subjects
-});
-
-// Results
 app.compare = new app.collections.URNList;
 app.results = new app.collections.results;
 
-// Global school view (results + compare)
-app.schools = new app.views.schools({
-  el: $('.schools'),
-  compare: app.compare,
-  $compare: $('.compare'),
-  results: app.results,
-  $results: $('.results')
+// We need the DOM for the next operations.
+
+$(function() {
+
+  var $body = $(document.body);
+
+  if ('geolocation' in navigator) {
+    $body.addClass('geolocation');
+  }
+
+  // scrolling performance problems
+  // $('.schools').find('table').stickyTableHeaders();
+
+  app.search = new app.views.search({
+    el: $('.search-form')
+  });
+  app.subjects.view = new app.views.subjects({
+    el: $('.subjects-filter'),
+    collection: app.subjects
+  });
+  app.schools = new app.views.schools({
+    el: $('.schools'),
+    compare: app.compare,
+    $compare: $('.compare'),
+    results: app.results,
+    $results: $('.results')
+  });
+  
+  app.router = new app.routers.main;
+
+  Backbone.history.start({pushState: true});
+
 });
-
-Backbone.history.start({pushState: true});
-
-// search();
-
-
-// })();
