5b47ffa80a56fe2d71e341b3d77c38f358671139
Daniel Gavrilov <drifter4e@gmail.com>
2014-02-09 17:12:44 +0000

    Better NewRelic integration?

--- app/index.js
+++ app/index.js
@@ -1,23 +1,16 @@
+if (process.env.NODE_ENV === 'production') var newrelic = require('newrelic');
 var express = require('express');
 var api = require('./api');
 var site = require('./site');
 
 var app = express();
 var port = process.env.PORT || 8000;
-var env = app.get('env');
 
 app.use(express.favicon());
-
-if (env === 'production') {
-  var newrelic = require('newrelic');
-  app.use(require('logfmt').requestLogger());
-} else {
-  app.use(express.logger('dev'));
-}
-
+app.use(require('logfmt').requestLogger());
 app.use(express.compress());
 app.use('/api', api);
-app.use(site);
+app.use('/', site);
 app.use(express.errorHandler());
 
 app.listen(port, function() {
