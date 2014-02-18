935933d4ee9fa7a0a3995bd3a5399d89f165976c
Daniel Gavrilov <drifter4e@gmail.com>
2014-02-18 19:22:36 +0000

    Light colors are slightly lighter

--- scripts/colors/colors.js
+++ scripts/colors/colors.js
@@ -7,26 +7,26 @@ module.exports = [
 {
   name:  "blue",
   dark:  "hsl(205, 78%, 54%)",
-  light: "hsl(205, 68%, 78%)"
+  light: "hsl(205, 68%, 80%)"
 },
 {
   name:  "green",
   dark:  "hsl(138, 83%, 38%)",
-  light: "hsl(138, 53%, 70%)"
+  light: "hsl(138, 53%, 72%)"
 },
 {
   name:  "orange",
   dark:  "hsl(28, 78%, 44%)",
-  light: "hsl(28, 68%, 74%)"
+  light: "hsl(28, 68%, 76%)"
 },
 {
   name:  "violet",
   dark:  "hsl(268, 68%, 58%)",
-  light: "hsl(268, 58%, 80%)"
+  light: "hsl(268, 58%, 82%)"
 },
 {
   name:  "brown",
   dark:  "hsl(10, 30%, 42%)",
-  light: "hsl(10, 20%, 68%)"
+  light: "hsl(10, 20%, 70%)"
 }
 ];
